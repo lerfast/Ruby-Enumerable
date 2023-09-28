f18995494414acdbdf8857eb28aa3b14a24d890d
Juan Diego Guerra <jdxelric@gmail.com>
2023-09-28 14:39:49 -0500

    Update my_enumerable.rb

---
+++ my_enumerable.rb
@@ -0,0 +1,10 @@
+require_relative 'my_enumerable'
+class MyList
+  include MyEnumerable
+  def initialize(*elements)
+    @list = elements
+  end
+  def each(&block)
+    @list.each(&block)
+  end
+end
